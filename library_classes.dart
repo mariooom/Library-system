dd1a8ecb3e4c56e9777922ebd04d2536de49bf41
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-12-22 02:42:04 +0200

    Update library_classes.dart

--- library_classes.dart
+++ library_classes.dart
@@ -1,10 +1,12 @@
-class Books {
+class Books { 
+  //attributes
   int? bookID;
   String? title;
   bool? borrowed;
-
+  //constructor
   Books({this.bookID, this.title, this.borrowed});
 
+  //method to display book info
   void displayBookInfo() {
     print("Book ID: $bookID");
     print("Title: $title");
@@ -13,9 +15,12 @@ class Books {
 }
 
 class User {
+  //attributes
   int? userID;
   String? name;
+  //constructor
   User({this.userID, this.name});
+  //method to display user info
   void displayUSerInfo() {
     print("User ID: $userID");
     print("Name: $name");
@@ -23,33 +28,38 @@ class User {
 }
 
 class Library {
+  //attributes
   List<Books> books = [];
   List<User> users = [];
+  //method to add books to library
   void addBook(Books book) {
-    books.add(book);
+    books.add(book);   //adding book
     print('Book" ${book.title} "has been added to library');
   }
-
+  //method to add users to library
   void addUser(User user) {
-    users.add(user);
+    users.add(user);  //adding user
     print('User" ${user.name} "has been added to library');
   }
-
+  //method to borrow books
   void borrowBook(Books book) {
+    //checking if book is borrowed
     if (book.borrowed == false) {
       print('Book" ${book.title} " can be borrowed');
     }
   }
-
+  //method to return books
   void returnBook(Books book) {
+    //checking if book is borrowed and should be returned
     if (book.borrowed == true) {
       print('Book" ${book.title} "should be returned');
     }
   }
-
+  //method to display library info
   void displayInfo() {
-    print("Number of users: ${users.length}");
-    print("Number of books: ${books.length}");
+    print("Number of users: ${users.length}");   //displaying number of users
+    print("Number of books: ${books.length}");  //displaying number of books
+    //displaying books info which were added to library
     for( var book in books) {
       book.displayBookInfo();
     }
